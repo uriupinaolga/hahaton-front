d0e77b5d082f0db6342c6ae6bcc68dbf59cb75c5
Nagowar <55431898+Nagowar@users.noreply.github.com>
2020-10-24 20:16:16 +0300

    added pallette

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,34 @@
               <input type="file" name="file" @change="onFileChange">
               <input type="submit" value="Submit">
           </form>
+<div class="image-response" v-if="response.cosmetic">
+    <img :src="image"/>
+    <p>Прогноз ремонта варианта ремонта</p>
+    <div>
+        <p>Косметический ремонт: {{response.cosmetic.toFixed(4)}}%</p>
+        <p>Без ремонта: {{response.without.toFixed(4)}}%</p>
+        <p>Люкс: {{response.luxury.toFixed(4)}}%</p>
+        <p>Обычный ремонт: {{response.standard.toFixed(4)}}%</p>
+    </div>
+    <div class="colors">
+        <div class="dom-color">
+            <p>Доминирующий цвет</p>
+            <span class="domin-color" :style="{backgroundColor: `rgb(${response.dominant[0]},${response.dominant[1]},${response.dominant[2]})`}"></span>
+        </div>
+        <div class="palette">
+            <p>Доп.цвета</p>
+            <div class="palette-colors">
+                  <span v-for="(color, i) in response.palette"
+                        :key="`color${i}`"
+                        class="child-color"
+                        :style="{backgroundColor: `rgb(${color[0]},${color[1]},${color[2]})`}"
+                  ></span>
+            </div>
+        </div>
+    </div>
+</div>
       </div>
+
     </div>
   </div>
 </template>
@@ -22,38 +49,38 @@ export default {
   name: 'App',
     data() {
       return {
-          img: '',
-          response: ''
+          image: '',
+          response: '',
+          res: {}
       }
     },
   methods: {
       onFileChange: function(e) {
-          let files = e.target.files || e.dataTransfer.files;
-          if (!files.length)
-              return;
-          this.createImage(files[0]);
+          this.image = e.target.files[0] || e.dataTransfer.files[0];
       },
-      createImage: function(file) {
-          let reader = new FileReader();
-          let vm = this;
-          reader.onload = function(e) {
-              vm.image = e.target.result;
-          };
-          reader.readAsDataURL(file);
-
+      async uploadImg() {
+          const that = this
+          let data = new FormData();
+          data.append('file', this.image);
+          axios.post('https://hahaton.ngrok.io/upload', data, {
+              headers: {
+                  'Content-Type': 'multipart/form-data'
+              }
+          })
+              .then(function(res) {
+                  that.response = res.data
+                  console.log(res)
+              })
+          .then(() => {
+              this.getImageUploaded()
+          })
       },
-     async uploadImg() {
-         let data = new FormData();
-         data.append('file.jpg', this.image);
-
-         axios.post('https://hahaton.ngrok.io/upload', data, {
-             headers: {
-                 ''
-             }
-         })
-             .then(function(res) {
-                 console.log(res)
-             });
+      async getImageUploaded() {
+          axios.get('https://hahaton.ngrok.io/image', {
+          })
+              .then(function(res) {
+                  console.log(res)
+              });
       }
 },
 
@@ -73,7 +100,10 @@ export default {
 }
 .wrapper-body {
 padding: 7% 15%;
-
+display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 }
 .header {
   width: 100vw;
@@ -89,7 +119,7 @@ padding: 7% 15%;
 .header p {
   color: white;
 }
-  button {
+  input[type = 'submit'] {
     transition: all 0.3s ease;
     background-color: black;
     color: white;
@@ -101,9 +131,42 @@ padding: 7% 15%;
     cursor: pointer;
     border: none;
   }
-  button:hover {
+  input[type = 'submit']:hover {
     background-color: darkgrey;
     color: black;
   }
 
+  .colors {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+  }
+.domin-color {
+    width: 50px;
+    height: 50px;
+    border-radius: 30px;
+}
+    .child-color {
+        margin-right: 10px;
+        width: 30px;
+        border-radius: 15px;
+        height: 30px;
+    }
+    .palette {
+       display: flex;
+        flex-direction: column;
+    }
+    .dom-color {
+        display: flex;
+        flex-direction: row;
+    }
+    .dom-color p {
+        margin-right: 10px;
+    }
+  .palette-colors {
+      display: flex;
+      flex-direction: row;
+      width: 100%;
+  }
 </style>
